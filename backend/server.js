214ba3f854ba96b239e5646a169f3f222275712f
Miguel Salas <salasmiguelsalas464@gmail.com>
2026-03-31 05:13:36 +0800

    Fix TiDB SSL and DB Connection

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,12 @@ app.use(express.json());
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
+    minVersion: "TLSv1.2",
     rejectUnauthorized: false
   }
 });
